f481b7ddfcbfd1dcf7b01831c9a6fb6eb40362ce
JoeFedora <220084115+aconmythics@users.noreply.github.com>
2025-08-19 21:06:00 +0100

    completed exercises 1 to 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(toRepeat, amount) {
+    let message = ""
+    if (amount < 0) {
+        return "ERROR"
+    }
+    for(let i = 0; i < amount; i++) {
+        message += toRepeat;
+    }
+    return message
 };
-
 // Do not edit below this line
 module.exports = repeatString;

6115dbbd8a1026c0ca782e81e4e5434e677de293
JoeFedora <220084115+aconmythics@users.noreply.github.com>
2025-08-25 20:34:31 +0100

    completed exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+// THERE IS A REVERSE FUNCTION BUILT IN AND THATS THE SOLUTION ARE YOU SERIOUS??? WHATS THE POINT IN THE EXERCISE THEN IF YOU WANT TO USE .REVERSE!!!!
+const reverseString = function(str) {
+    str.split()
+    let reverse = []
+    for (i of str) {
+        reverse.unshift(i)
+    }
+    return reverse.join("")
 };
 
 // Do not edit below this line

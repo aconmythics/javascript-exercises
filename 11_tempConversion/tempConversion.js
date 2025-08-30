889fd09bc89e3d2ef6900cefe55ceae96480005a
JoeFedora <220084115+aconmythics@users.noreply.github.com>
2025-08-30 01:01:16 +0100

    completed exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celsius = (temp-32)*(5/9)
+  return parseFloat(celsius.toFixed(1)) 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahrenheit = ((temp * (9/5))+32)
+  return parseFloat(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line

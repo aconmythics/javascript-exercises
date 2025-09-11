e50111b4b2a80cf7552b7fd3b98e15aabcccf198
JoeFedora <220084115+aconmythics@users.noreply.github.com>
2025-09-11 18:19:46 +0100

    js exercise 12 complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  arrSum = 0
+	array.forEach(element => {
+    arrSum += element
+  });
+  return arrSum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0 || a == 1) {
+    return 1
+  }
+  let ans = 1
+  for (i = a; i>0;i--) {
+    ans *= i
+  };
+  return ans;
 };
 
 // Do not edit below this line

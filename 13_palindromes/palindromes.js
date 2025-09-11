fc0ebcd04320e9a226b4c93d53ff1cbe2f22298f
JoeFedora <220084115+aconmythics@users.noreply.github.com>
2025-09-11 20:44:04 +0100

    completed exercise 13 and 14

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    stripped = palindrome.replace(/[^a-zA-Z0-9]/g, '').split("")
+    if (stripped.join().toLowerCase() == stripped.reverse().join().toLowerCase()) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS";
+    }
+    let ans = 0
+    prev = 1
+    for (i = 0; i < a; i++) {
+        let temp = ans;
+        ans += prev;
+        prev = temp;
+    }
+    return ans;
 };
+
+fibonacci(4)
 
 // Do not edit below this line
 module.exports = fibonacci;
